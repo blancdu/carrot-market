c02705d3f25b21d0c9922805f198a2a1758d01b7
blancdu <hongsj36@gmail.com>
2024-04-15 21:15:04 +0900

    useFormState

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -17,7 +17,7 @@ export default function FormInput({
     <div className="flex flex-col gap-2">
       <input
         name={name}
-        className="bg-transparent rounded-md px-2 w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         type={type}
         placeholder={placeholder}
         required={required}
